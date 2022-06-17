90dc03f188d77a404ffe249dce3ada1871424846
kelly <kellybackwards@gmail.com>
2022-06-17 17:00:51 -0400

    added svg icon to prep dark mode toggle

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -76,7 +76,15 @@
     </div>
 
     <div class="flex w-20 justify-end">
-        <button class="px-1" title="Toggle Theme">T</button>
+        <button class="px-1" @click="toggleDark" title="Toggle Theme">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 hover:animate-spin" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+            </svg>
+        </button>
     </div>
 </template>
 
@@ -88,6 +96,10 @@ const props = defineProps({
 })
 
 const isOpen = ref(false)
+
+function toggleDark() {
+    console.log("clicked moon")
+}
 
 </script>
 
