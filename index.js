e963e71aaeb769203d8bd9ea348152b9fe20522e
kelly <kellybackwards@gmail.com>
2022-06-23 16:29:08 -0400

    wordlist reading from json instead of api call; b4 removing backend

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const PORT = 8000
-const axios = require("axios").default
+
 const express = require("express")
 const cors = require("cors")
-require('dotenv').config()
+// require('dotenv').config()
 const app = express()
-
 app.use(cors())
 
+let words = require("./words.json")
+const wordArray = words['words']
+
+
 app.get('/word', (req, res) => {
-    const options = {
-        method: 'GET',
-        url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
-        params: {count: '5', wordLength: '5'},
-        headers: {
-            'X-RapidAPI-Host': 'random-words5.p.rapidapi.com',
-            'x-RapidAPI-Key': process.env.RAPID_API_KEY
-        }
-    }
-    axios.request(options).then((response) => {
-        console.log(response.data)
-        res.json(response.data[0])
-    }).catch((error) => {
-        console.error(error)
-    })
+    //generate random number
+    let randIndex = Math.floor(Math.random() * wordArray.length) //# of words in json
+    console.log("randIndex: ", randIndex)
+    //grab that index from word array
+    let ranWord = wordArray[randIndex]
+    console.log("ranWord: ", ranWord)
+    res.json(ranWord)
 })
 
-app.listen(PORT, () => console.log('Server running on port ' + PORT))
+
+
+app.listen(PORT, () => { 
+    console.log('Server running on port ' + PORT)
+})
