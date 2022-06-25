185c015a38cfb5351d18f0a99d57c89ecabe600d
kelly <kellybackwards@gmail.com>
2022-06-24 21:39:31 -0400

    imgs static assets; fix github logo on darkmode

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,24 @@
+<script setup>
+import { ref, defineProps } from "vue";
+
+const gitImg = new URL("../assets/git.png", import.meta.url).href
+
+const emit = defineEmits(["toggleDark"])
+
+const props = defineProps({
+    isOpen: Boolean,
+    darkmode: Boolean,
+})
+
+const isOpen = ref(false)
+
+function toggleDark() {
+    console.log("clicked moon before emit")
+    emit("toggleDark")
+}
+
+</script>
+
 <template>
     <div class="flex items-center w-20 justify-start">
         <!-- class= menu-left -->
@@ -64,18 +85,18 @@
                             The letter tiles will change color depending on how close your guess was!<br>
                             <br>
                             <span style="color: #16a34a; font-weight:bold">GREEN</span> tiles indicate that the letter is in the word and in the correct spot.<br>
-                            <img src="https://i.imgur.com/ZGyKEke.png" alt="Green tile means letter is in the word and right spot" title="Green letter tile" /><br>
+                            <img src="../assets/green.png" alt="Green tile means letter is in the word and right spot" title="Green letter tile" /><br>
                             <span style="color: #eab308; font-weight:bold">YELLOW</span> tiles indicate that the letter is in the word but in the incorrect spot.<br>
-                            <img src="https://i.imgur.com/IUIOi5F.png" alt="Yellow tile means letter is in word but wrong spot" title="Yellow letter tile" /><br>
+                            <img src="../assets/yellow.png" alt="Yellow tile means letter is in word but wrong spot" title="Yellow letter tile" /><br>
                             <span style="color: #6b7280; font-weight:bold">GRAY</span> tiles mean that the letter is not in the word in any spot.<br>
-                            <img src="https://i.imgur.com/qoMOkIC.png" alt="Gray tile means letter is not in the word at all" title="Gray letter tile" /><br>
+                            <img src="../assets/gray.png" alt="Gray tile means letter is not in the word at all" title="Gray letter tile" /><br>
                             <br>
                             Hit <b>Play Again</b> to guess a different word!
                         </p>
                     </div>
                     <!-- footer -->
                     <div class="flex items-center justify-between p-6 border-t border-solid border-slate-200 rounded-b">
-                        <a href="https://github.com/kellyjellykitten/wordingo" target="_blank"><img alt="Source Code" class="w-11 h-11 object-left hover:animate-spin" title="View Source Code on GitHub" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"></a>
+                        <a href="https://github.com/kellyjellykitten/wordingo" target="_blank"><img alt="Source Code" class="w-11 h-11 object-left hover:animate-spin" title="View Source Code on GitHub" src="../assets/gitlight.png"></a>
                         <button class="text-green-600 bg-transparent border border-solid border-green-600 hover:bg-green-600 hover:text-white active:bg-green-700 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" @click="isOpen = false">
                             Ok, let's play!
                         </button>
@@ -113,32 +134,18 @@
                             The letter tiles will change color depending on how close your guess was!<br>
                             <br>
                             <span style="color: #16a34a; font-weight:bold">GREEN</span> tiles indicate that the letter is in the word and in the correct spot.<br>
-                            <img src="https://i.imgur.com/ZGyKEke.png" alt="Green tile means letter is in the word and right spot" title="Green letter tile" /><br>
+                            <img src="../assets/green.png" alt="Green tile means letter is in the word and right spot" title="Green letter tile" /><br>
                             <span style="color: #eab308; font-weight:bold">YELLOW</span> tiles indicate that the letter is in the word but in the incorrect spot.<br>
-                            <img src="https://i.imgur.com/IUIOi5F.png" alt="Yellow tile means letter is in word but wrong spot" title="Yellow letter tile" /><br>
+                            <img src="../assets/yellow.png" alt="Yellow tile means letter is in word but wrong spot" title="Yellow letter tile" /><br>
                             <span style="color: #6b7280; font-weight:bold">GRAY</span> tiles mean that the letter is not in the word in any spot.<br>
-                            <img src="https://i.imgur.com/qoMOkIC.png" alt="Gray tile means letter is not in the word at all" title="Gray letter tile" /><br>
+                            <img src="../assets/gray.png" alt="Gray tile means letter is not in the word at all" title="Gray letter tile" /><br>
                             <br>
                             Hit <b>Play Again</b> to guess a different word!
                         </p>
                     </div>
                     <!-- footer -->
                     <div class="flex items-center justify-between p-6 border-t border-solid border-slate-200 rounded-b">
-                        <!-- <a href="https://github.com/kellyjellykitten/wordingo" target="_blank"><img alt="Source Code" class="w-11 h-11 object-left hover:animate-spin" title="View Source Code on GitHub" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"></a> -->
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlns:xlink="http://www.w3.org/1999/xlink"
-                            class="w-11 h-11 object-left hover:animate-spin"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <a xlink:href="https://github.com/kellyjellykitten/wordingo">
-                            <path
-                                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-                            />
-                            </a>
-                        </svg>
+                        <a href="https://github.com/kellyjellykitten/wordingo" target="_blank"><img alt="Source Code" class="w-11 h-11 object-left hover:animate-spin" title="View Source Code on GitHub" src="../assets/git.png"></a>
                         <button class="text-green-600 bg-transparent border border-solid border-green-600 hover:bg-green-600 hover:text-white active:bg-green-700 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" @click="isOpen = false">
                             Ok, let's play!
                         </button>
@@ -175,24 +182,6 @@
     </div>
 </template>
 
-<script setup>
-import { ref, defineProps } from "vue";
-
-const emit = defineEmits(["toggleDark"])
-
-const props = defineProps({
-    isOpen: Boolean,
-    darkmode: Boolean,
-})
-
-const isOpen = ref(false)
-
-function toggleDark() {
-    console.log("clicked moon before emit")
-    emit("toggleDark")
-}
-
-</script>
 
 <style>
 </style>
